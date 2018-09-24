35ba5157f8b7dad1677fd5202285d44fd270972b
superdevelopertr <superdevelopertr@outlook.com>
2018-09-24 19:42:00 +0300

    comments added

--- test/com/ailhanli/moneytransfer/controller/test/TransferControllerTest.java
+++ test/com/ailhanli/moneytransfer/controller/test/TransferControllerTest.java
@@ -32,51 +32,61 @@ public class TransferControllerTest {
 	
 	@Test
 	public void test_successTransfer() {
+		//creating source and destination account for transfer
 		Account sourceAccount = new Account("Abdullah", 1000, Currency.getInstance("EUR"));
 		Account destinationAccount = new Account("Omer", 500, Currency.getInstance("EUR"));
 		
 		given().body(sourceAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		given().body(destinationAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		
+		//getting source and destination account id
 		Integer sourceAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Abdullah\"}.accountId");
 		Integer destinationAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Omer\"}.accountId");
 		
+		//transfer api call
 		Transfer transfer = new Transfer(sourceAccountId, destinationAccountId,100.0, Currency.getInstance("EUR"),"for transfer service test 1");
 		given().body(transfer).post(API_KEY).then().assertThat().statusCode(200);
 	}
 	
 	@Test
 	public void test_TransferForNonExistingAccount() {	
+		//testing for non existing account
 		Transfer transfer = new Transfer(87, 1,100.0, Currency.getInstance("EUR"),"for transfer service test 2");
 		given().body(transfer).post(API_KEY).then().assertThat().statusCode(404);
 	}
 	
 	@Test
 	public void test_TransferForInsufficientBalance() {	
+		//creating source and destination account for transfer
 		Account sourceAccount = new Account("Abdullah", 1000, Currency.getInstance("EUR"));
 		Account destinationAccount = new Account("Omer", 500, Currency.getInstance("EUR"));
 		
 		given().body(sourceAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		given().body(destinationAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		
+		//getting source and destination account id
 		Integer sourceAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Abdullah\"}.accountId");
 		Integer destinationAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Omer\"}.accountId");
 		
+		//transfer insufficient balance api call
 		Transfer transfer = new Transfer(sourceAccountId, destinationAccountId,9999.0, Currency.getInstance("EUR"),"for transfer service test 1");
 		given().body(transfer).post(API_KEY).then().assertThat().statusCode(404);
 	}
 	
 	@Test
-	public void test_TransferForNegativeTransfer() {	
+	public void test_TransferForNegativeTransfer() {
+		//creating source and destination account for transfer
 		Account sourceAccount = new Account("Abdullah", 1000, Currency.getInstance("EUR"));
 		Account destinationAccount = new Account("Omer", 500, Currency.getInstance("EUR"));
 		
 		given().body(sourceAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		given().body(destinationAccount).post(ACCOUNT_API_KEY).then().assertThat().statusCode(200);
 		
+		//getting source and destination account id
 		Integer sourceAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Abdullah\"}.accountId");
 		Integer destinationAccountId = get(ACCOUNT_API_KEY).then().assertThat().statusCode(200).extract().jsonPath().getInt("find {it.name==\"Omer\"}.accountId");
 		
+		//transfer negative amount api call
 		Transfer transfer = new Transfer(sourceAccountId, destinationAccountId,-100.0, Currency.getInstance("EUR"),"for transfer service test 1");
 		given().body(transfer).post(API_KEY).then().assertThat().statusCode(404);
 	}
